71d219cd439f820928b191f4af4129057ec37d92
Kyle Carhart <77036988+kylecarhart@users.noreply.github.com>
2025-02-08 09:11:58 -0500

    init shadcn

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,19 @@
 // vite.config.ts
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
-import viteReact from "@vitejs/plugin-react";
+import react from "@vitejs/plugin-react";
+import path from "path";
 import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     TanStackRouterVite({ autoCodeSplitting: true }),
-    viteReact(),
+    react(),
     // ...,
   ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 });
