f4b7664e8121d7364439ce886051cdf1f8eb70f8
Kyle Carhart <77036988+kylecarhart@users.noreply.github.com>
2025-02-12 12:07:04 -0500

    debug client on mobile

--- src/clients/client.ts
+++ src/clients/client.ts
@@ -22,8 +22,9 @@ async function request(endpoint: string, options: RequestInit = {}) {
 
   // If user is not logged in, reset state and reload.
   if (res.status === 401) {
-    localStorage.removeItem("auth.user");
-    window.location.reload();
+    alert("Unauthorized");
+    // localStorage.removeItem("auth.user");
+    // window.location.reload();
     // throw new UnauthorizedError();
   } else if (!res.ok) {
     throw res;
